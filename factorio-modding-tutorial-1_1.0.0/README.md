81465922f589b662fa695a922968918c5b32030c
mzmpy <mznpy@users.noreply.github.com>
2020-10-05 00:25:56 +0900

    Add tutorial 1 for Factorio v1.0.0

---
+++ factorio-modding-tutorial-1_1.0.0/README.md
@@ -0,0 +1,5 @@
+# Tutorial 1
+
+- Replaced iron-plate image with other one.
+
+- 鉄板の画像を変更
---
+++ factorio-modding-tutorial-1_1.0.0/item.lua
@@ -0,0 +1,12 @@
+--item.lua
+data:extend(
+    {
+        {
+            type = "item",
+            name = "iron-plate",
+            icon = "__factorio-modding-tutorial-1__/graphics/icons/iron-plate.png",
+            icon_size = 400,
+            stack_size = 100,
+        },
+    }
+)
